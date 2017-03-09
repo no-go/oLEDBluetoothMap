d8981e63135ec90dc9971dd276afed530a3a840a
Socken Real <socken@macblock>
2017-03-09 03:33:02 +0100

    start ui

--- WebsocketByte2PNG/WebsocketByte2PNG.go
+++ WebsocketByte2PNG/WebsocketByte2PNG.go
@@ -5,13 +5,39 @@ import (
 	"log"
 	"net/http"
 	"io/ioutil"
+	
+	"fmt"
 
 	"github.com/gorilla/websocket"
+	"github.com/andlabs/ui"
 )
 
 var addr = flag.String("addr", "0.0.0.0:65000", "http service address")
 
 var upgrader = websocket.Upgrader{} // use default options
+
+var box = ui.NewVerticalBox()
+//var windowExists = false
+var window = ui.NewWindow("WebsocketByte2PNG", 200, 100, false)
+var messages = 0
+
+func popUp(msg string) {
+	messages++
+	box = ui.NewVerticalBox()
+	box.Append(ui.NewLabel(fmt.Sprintf("%s %d",msg, messages)), false)
+	window.SetChild(box)
+	//if (windowExists == false) {
+		ui.Main(func() {
+	//		windowExists = true
+			window.OnClosing(func(*ui.Window) bool {
+				//windowExists = false
+				ui.Quit()
+				return true
+			})
+			window.Show()
+		})
+	//}
+}
 
 func echo(w http.ResponseWriter, r *http.Request) {
 	c, err := upgrader.Upgrade(w, r, nil)
@@ -27,6 +53,7 @@ func echo(w http.ResponseWriter, r *http.Request) {
 		}
 		//log.Printf("recv")
 		err = ioutil.WriteFile("out.png", []byte(message), 0644)
+		popUp("new message")
 		if err != nil {
 			log.Println("error:", err)
 			break
